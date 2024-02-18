31a684e6a79db62576441648518f42c6b28f742e
abedsully <abedsully2909@gmail.com>
2024-02-18 21:52:24 +0700

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Stefanus Albert Wilson on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
